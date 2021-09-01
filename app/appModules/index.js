3ad576452856aa1496082e7417e1671030223548
easy4docker <easy4docker@gmail.com>
2021-09-01 14:14:25 -0700

    Update index.js

--- app/appModules/index.js
+++ app/appModules/index.js
@@ -1,35 +1,29 @@
 module.exports = (req, res, next)=> {
+  const standardModule = (module,req, res, next)=> {
+    delete require.cache[__dirname +'/' + module + '.js'];
+    const M = require(__dirname +'/' + module + '.js');
+    const m = new M(req, res, next);
+    if (!!req.body.action && typeof m[req.body.action] === 'function') {
+      m[req.body.action]();
+    } else {
+      m.actionError();
+    }
+  }
+
   switch (req.params.module) {
     case 'application':
-      delete require.cache[__dirname +'/application.js'];
-      const Application = require(__dirname +'/application.js');
-      const application = new Application(req, res, next);
-      if (!!req.body.action && typeof application[req.body.action] === 'function') {
-        application[req.body.action]();
-      } else {
-        application.actionError();
-      }
-      break
     case 'community':
-      delete require.cache[__dirname +'/community.js'];
-      const Community = require(__dirname +'/community.js');
-      const community = new Community(req, res, next);
-      if (!!req.body.action && typeof community[req.body.action] === 'function') {
-        community[req.body.action]();
-      } else {
-        community.actionError();
-      }
-      break
-
     case 'menu':
+      standardModule(req.params.module, req, res, next);
+      /*
       delete require.cache[__dirname +'/menu.js'];
       const Menu = require(__dirname +'/menu.js');
       const menu = new Menu(req, res, next);
       if (!!req.body.action && typeof menu[req.body.action] === 'function') {
-        community[req.body.action]();
+        menu[req.body.action]();
       } else {
-        community.actionError();
-      }
+        menu.actionError();
+      }*/
       break
 
     default: 
